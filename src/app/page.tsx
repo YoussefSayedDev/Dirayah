0971e542f38a3e6fe100080c9410f0ac0fe5421a
YoussefSayedDev <youssef.el.sayed.email@gmail.com>
2025-03-07 21:24:51 +0200

    Installed the deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <h1 className='text-3xl font-bold'>Welcome to StudyFlow</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1 className="text-3xl font-bold">Welcome to StudyFlow</h1>
     </div>
   );
 }
